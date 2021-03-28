7acdd97924a73d4450886748ee4f2a2122b3621e
John Doe <wicaksanasadewa@yahoo.com>
2021-03-28 13:23:03 +0700

    + Change the icon launcher

--- app/src/main/java/com/codedirect/footballapps/MainActivity.kt
+++ app/src/main/java/com/codedirect/footballapps/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.codedirect.footballapps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
