c7fcca4cdaefb986c51f25e7c09390300287591c
John Doe <wicaksanasadewa@yahoo.com>
2021-03-28 13:58:05 +0700

    + Change the main activity

--- app/src/main/java/com/codedirect/footballapps/MainActivity.kt
+++ app/src/main/java/com/codedirect/footballapps/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.codedirect.footballapps
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //public variable to change the data
+    private var startPoint = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +18,18 @@ class MainActivity : AppCompatActivity() {
         //mengubah text id yang ada ke tulisan yg diinginkan
         /*tv_main_title.text = "Hello Sadewa!"
         tv_main_title.textSize = 30.0f //float ada tambahan f dibelakang nilai*/
+    }
+
+    fun adding(v: View) {
+        startPoint = startPoint.plus(1)
+        tv_main_value.text = startPoint.toString()
+    }
+    fun minus(v: View) {
+        startPoint = startPoint.minus(1)
+        tv_main_value.text = startPoint.toString()
+    }
+    fun reset(v: View) {
+        startPoint = 0
+        tv_main_value.text = startPoint.toString()
     }
 }
